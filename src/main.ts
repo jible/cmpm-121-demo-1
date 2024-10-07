7d820aa5aed8d5e7bf705949a9c8f97871861fe3
jible <jamezman88th@gmail.com>
2024-10-07 14:49:23 -0700

    added button with counter (Step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,17 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+let count = 0;
+
+const countDisplay = document.createElement("countDisplay");
+countDisplay.innerHTML = `${count}`;
+document.body.appendChild(countDisplay);
+
+const button = document.createElement("button");
+button.innerHTML = "Click Me";
+
+document.body.appendChild(button);
+button.addEventListener("click", () => {
+  count++;
+  countDisplay.innerHTML = `${count}`;
+});
