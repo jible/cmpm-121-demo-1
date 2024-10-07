3199a7f9aa10557d1050b78dc00a20f3d855431b
jible <jamezman88th@gmail.com>
2024-10-07 15:15:36 -0700

    step 1 refined and step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,30 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My stupendous game";
+const gameName = "Free Money the Game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-let count = 0;
+
+let money_count = 0;
+//let all_time_money = 0;
 
 const countDisplay = document.createElement("countDisplay");
-countDisplay.innerHTML = `${count}`;
-document.body.appendChild(countDisplay);
+countDisplay.innerHTML = `${money_count}`;
+app.append(countDisplay);
 
 const button = document.createElement("button");
-button.innerHTML = "Click Me";
+button.innerHTML = "💸";
 
-document.body.appendChild(button);
+app.append(button);
 button.addEventListener("click", () => {
-  count++;
-  countDisplay.innerHTML = `${count}`;
+  money_count++;
+  countDisplay.innerHTML = `${money_count}`;
 });
+
+setInterval(() => {
+  money_count++;
+  countDisplay.innerHTML = `${money_count}`;
+}, 1000);
