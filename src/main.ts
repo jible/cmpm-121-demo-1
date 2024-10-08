31d714de65ff0c445cfda3981289cfae4fa8d5c2
jible <jamezman88th@gmail.com>
2024-10-08 14:22:07 -0700

    Swapping to pc

--- src/main.ts
+++ src/main.ts
@@ -25,14 +25,78 @@ money_button.addEventListener("click", () => {
   money_count++;
 });
 
+
+
+
+class building {
+  name: string;
+  baseMps: number;
+  cost: number;
+  costIncreaseRate: number = 1.2;
+  buildingCount: number = 0;
+
+
+  constructor(name: string, baseMPS: number, cost: number, costIncreaseRate?: number){
+    this.name = name;
+    this.baseMps = baseMPS;
+    this.cost = cost;
+    this.buildingCount = 0;
+    if (costIncreaseRate){
+      this.costIncreaseRate = costIncreaseRate;
+    }
+  }
+}
+
+// Make Buildings
+const buildingArray: building[] = [];
+const lemonadeStand = new building("Lemonade Stand", .1, 10);
+const minimumWageJob = new building("Minimum Wage Job", 2, 100);
+const goldenGoose = new building("Golden Goose", 50, 1000);
+
+
+
+
+// investment property
+// bank
+// cookie converter
+//money mint
+// money solar panel?
+// do chores
+// money waterfall
+// golden goose
+// 
+// buy a buisness
+// infinite money glitch
+buildingArray.push(lemonadeStand);
+buildingArray.push(minimumWageJob);
+buildingArray.push(goldenGoose);
+
+// Make buttons for buildings
+
+
+const lemonadeStandButton = document.createElement("button");
+lemonadeStandButton.innerHTML =
+  'Lemonade Stands: 0<br>Cost: 10';
+lemonadeStandButton.disabled = true;
+
+
+const minimumWageJobButton = document.createElement("button");
+minimumWageJobButton.innerHTML = 
+  'Minimum Wage Jobs: 0<br>Cost: 50';
+minimumWageJobButton.disabled = true;
+
+const goldenGooseButton = document.createElement("button");
+goldenGooseButton.innerHTML = 
+  'Golden Geese: 0<br>Cost: 1000';
+goldenGooseButton.disabled = true;
+
+
+
 //Setting up purchase button
-const lemonade_stand_button = document.createElement("button");
-lemonade_stand_button.innerHTML =
-  "Lemonade Stands: 0\n Lemonade Stand Cost: 10";
-lemonade_stand_button.disabled = true;
-app.append(lemonade_stand_button);
-lemonade_stand_button.addEventListener("click", () => {
-  lemonade_stand_button.innerHTML = `Lemonade Stands: ${lemonade_stand_count}\n Lemonade Stand Cost: 10`;
+
+app.append(lemonadeStandButton);
+lemonadeStandButton.addEventListener("click", () => {
+  lemonadeStandButton.innerHTML = `Lemonade Stands: ${lemonade_stand_count}\n Lemonade Stand Cost: 10`;
   money_count -= 10;
   lemonade_stand_count += 1;
 });
@@ -43,15 +107,39 @@ requestAnimationFrame(step);
 function step(currentTime: DOMHighResTimeStamp) {
   const elapsed = currentTime - lastFrame;
   lastFrame = currentTime;
-  money_count += (lemonade_stand_count * elapsed) / 1000;
-  countDisplay.innerHTML = `${Math.round(money_count)}`;
+  const mps = calcMps()
+  
+
+  // Increase count
+  money_count += mps * ( elapsed/1000)
+  
+
 
   // update if things can be purchased
   if (money_count >= 10) {
-    lemonade_stand_button.disabled = false;
+    lemonadeStandButton.disabled = false;
   } else {
-    lemonade_stand_button.disabled = true;
+    lemonadeStandButton.disabled = true;
   }
 
+
+
+  // update display
+  countDisplay.innerHTML = `${Math.round(money_count)}`;
+
   requestAnimationFrame(step);
+}
+
+
+
+function calcMps( ){
+  let mps: number = 0;
+  for (let buildingIndex = 0; buildingIndex < buildingArray.length; buildingIndex++) {
+    const currentBuilding = buildingArray[buildingIndex]
+    
+    mps += currentBuilding.baseMps * 1
+  } 
+  
+  return mps
+
 }
