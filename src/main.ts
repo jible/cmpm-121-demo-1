2f545a8ed9458fee16a1c3ba7c9a0f77c608aad1
jible <jamezman88th@gmail.com>
2024-10-11 01:55:09 -0700

    Added visual assets and added descriptions for each building (haven't implemented them into the page yet)

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ class building {
   name: string;
   baseMps: number;
   cost: number;
+  description: string;
   costIncreaseRate: number = 1.15;
   buildingCount: number = 0;
   count: number = 0;
@@ -36,12 +37,14 @@ class building {
     name: string,
     baseMPS: number,
     cost: number,
+    description: string,
     costIncreaseRate?: number
   ) {
     this.name = name;
     this.baseMps = baseMPS;
     this.cost = cost;
     this.buildingCount = 0;
+    this.description = description;
     if (costIncreaseRate) {
       this.costIncreaseRate = costIncreaseRate;
     }
@@ -65,7 +68,6 @@ class building {
   updateButton() {
     if (this.button) {
       this.button.innerHTML = `${this.name}s: ${this.count}<br>Cost: ${Math.round(this.cost)}`;
-
       if (money_count >= this.cost) {
         this.button.disabled = false;
       } else {
@@ -111,17 +113,53 @@ const mpsDisplay: HTMLDivElement = makeUI();
 
 // Make Buildings
 const buildingArray: building[] = [];
-const lemonadeStand = new building("Lemonade Stand", 0.1, 10);
-const minimumWageJob = new building("Minimum Wage Job", 2, 100);
-const moneyTree = new building("Money Tree", 50, 1000);
-const goldenGoose = new building("Golden Goose", 120, 9000);
-const investmentProperty = new building("Investment Property", 500, 80000);
-const moneyMint = new building("Money Mint", 13000, 600000);
-const monopoly = new building("Monopoly", 50000, 3000000);
+const lemonadeStand = new building(
+  "Lemonade Stand",
+  0.1,
+  10,
+  "When life gives you lemons, make money! <br> Sells a 10 cent lemonade every second"
+);
+const minimumWageJob = new building(
+  "Minimum Wage Job",
+  2,
+  100,
+  `A classic way to make money!`
+);
+const moneyTree = new building(
+  `Money Tree`,
+  50,
+  1000,
+  `This stuff doesn't grow on... I guess it does!`
+);
+const goldenGoose = new building(
+  `Golden Goose`,
+  120,
+  9000,
+  `Poops gold! Sounds painful`
+);
+const investmentProperty = new building(
+  `Investment Property`,
+  500,
+  80000,
+  `Location Location Location!`
+);
+const moneyMint = new building(
+  `Money Mint`,
+  13000,
+  600000,
+  `Why don't more people just do this?`
+);
+const monopoly = new building(
+  `Monopoly`,
+  50000,
+  3000000,
+  `This doesn't feel ethical but look at all of this money!`
+);
 const infiniteMoneyGlitch = new building(
-  "Infinite Money Glitch",
+  `Infinite Money Glitch`,
   999999,
-  9999999999
+  9999999999,
+  `If you clip one dollar bill into another it does this and now you can't stop it.`
 );
 // const  = new building("", ,);
 // bank
